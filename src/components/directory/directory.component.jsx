279865953f15d81a77812fe745790fb75659e643
Sebastian Morgado <smorgado@uade.edu.ar>
2020-02-20 23:03:48 -0300

    class component menus

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+constructor(){
+    super();
+
+}
+
+}
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './menu-items.styles.scss';
+const MenuItem = ({ title }) => (
+    <div className='menu-item'>
+        <div className='content'>
+            <h1 className='title'>
+                {title}}
+                    </h1>
+            <span className='subtitle'>Comprar Ahora</span>
+        </div>
+    </div>
+)
+export default MenuItem;
