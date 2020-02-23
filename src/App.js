29d08d6e60ab40d9af0c22714aa92d0d39601e2e
Sebastian Morgado <smorgado@uade.edu.ar>
2020-02-23 11:59:36 -0300

    domingo de react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
+const SombrerosPage = () =>(
+<div>
+  <h1>Sombreros Page</h1>
+</div>
+);
 function App() {
   return (
     <div >
     <HomePage></HomePage>
-    </div>
+    </divimport { BrowerRouter } from 'react-router-dom';>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowerRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowerRouter>
+        <App />,
+    </BrowerRouter>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    document.getElementById('root')
+);
