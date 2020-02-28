03e743be5d3cfac06edbb9f9d3fd82efb33e95fb
Sebastian Morgado <smorgado@uade.edu.ar>
2020-02-27 22:58:47 -0300

    firebase usuario storage y get para state logueado

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth , createUserProfileDocument} from "./firebase/firebase.utils";
 import { Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
@@ -19,10 +19,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,21 +1,48 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-const config =  {
-    apiKey: "AIzaSyBCIw4lkOqmVpTQqc4ftUZRM9xif4_vITQ",
-    authDomain: "carro-db.firebaseapp.com",
-    databaseURL: "https://carro-db.firebaseio.com",
-    projectId: "carro-db",
-    storageBucket: "carro-db.appspot.com",
-    messagingSenderId: "915907816183",
-    appId: "1:915907816183:web:52e57b36d8458e552701f2",
-    measurementId: "G-RHDJYEWHZQ"
-  };
+const config = {
+  apiKey: "AIzaSyBCIw4lkOqmVpTQqc4ftUZRM9xif4_vITQ",
+  authDomain: "carro-db.firebaseapp.com",
+  databaseURL: "https://carro-db.firebaseio.com",
+  projectId: "carro-db",
+  storageBucket: "carro-db.appspot.com",
+  messagingSenderId: "915907816183",
+  appId: "1:915907816183:web:52e57b36d8458e552701f2",
+  measurementId: "G-RHDJYEWHZQ"
+};
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 firebase.initializeApp(config);
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
 export default firebase;
