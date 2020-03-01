6b979b8d0c8ceb9c9c3af99d205d4ca1723bac43
Sebastian Morgado <smorgado@uade.edu.ar>
2020-03-01 19:04:11 -0300

    Incorporo Redux + reducers + actions en logueo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,22 +1,40 @@
-import React from "react";
-import "./header.styles.scss";
-import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
-import { auth } from "../../firebase/firebase.utils";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { auth } from '../../firebase/firebase.utils';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
+
 const Header = ({ currentUser }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo"></Logo>
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        COMPRAR
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
       </Link>
-      <Link className="option" to="/contact">
-        CONTACTO
+      <Link className='option' to='/shop'>
+        CONTACT
       </Link>
-      {currentUser ? <div className='option' onClick={()=> auth.signOut()}>SALIR</div> : <Link className="option" to="/signin">INGRESAR</Link>}
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SALIR
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          INGRESAR
+        </Link>
+      )}
     </div>
   </div>
 );
-export default Header;
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
