53a3986ca56dba4b43e2cba3efc58a2a4b80b642
Sebastian Morgado <smorgado@uade.edu.ar>
2020-03-03 23:01:07 -0300

    add item con cantidad

--- src/redux/carrito/cart.reducer.js
+++ src/redux/carrito/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-
+import { addItemToCart } from './cart.utils';
 const INITIAL_STATE = {
   hidden: true,
   cartItems:[]
@@ -13,10 +13,10 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         hidden: !state.hidden
       };
       case CartActionTypes.ADD_ITEM:
-        return {
-          ...state,
-          cartItems: [...state.cartItems,action.payload]
-        }
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
     default:
       return state;
   }
---
+++ src/redux/carrito/cart.utils.js
@@ -0,0 +1,22 @@
+//si ya existe el item no lo agrego pero incremento la cantidad --
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+  //si existe cartItem entonces no agrego
+  if (existingCartItem) {
+    //map devuelve un nuevo array y incremento cant en uno si existe el item si no devuelvo el item como vino
+    console.table(cartItems);
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, cantidad: cartItem.cantidad + 1 }
+        : { cartItem }
+    );
+   
+  }
+ 
+  //si no encontro nada armo un nuevo array con la info y la cantidad hardcode en 1
+
+  return [...cartItems,{...cartItemToAdd, cantidad: 1 }]
+  //ahora importar esta fn en el reducer...
+};
