43c06d52f15e7d4c8c0425e798ea072087116d98
Sebastian Morgado <smorgado@uade.edu.ar>
2020-03-03 23:17:38 -0300

    check carro

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
+
+import CartItem from '../cart-item/cart-item.component';
+//hay que hacer un pulldown desde CartItem por eso hacemos connect
+import { connect } from 'react-redux';
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = () => (
+const CartDropdown = ( { cartItems } ) => (
   <div className="cart-dropdown">
+    <div className='cart-items'>
+      {cartItems.length ? (
+        cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className='empty-message'>Carrito Vacio</span>
+      )}
+    </div>
     <div className="cart-items">
-        <CustomButton>IR A CHECKOUT</CustomButton>
+        <CustomButton>IR AL CHECKOUT</CustomButton>
     </div>
   </div>
 );
-export default CartDropdown;
+const mapStateToProps = ({ cart: {cartItems} }) => (
+  {cartItems}
+)
+
+export default connect(mapStateToProps)(CartDropdown);
 
