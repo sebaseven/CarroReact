10edc5e12a0153c71bcefd4864b9bf639dbe0455
Sebastian Morgado <smorgado@uade.edu.ar>
2020-03-07 18:27:08 -0300

    reselctor para memorizar cantidad sin re renderizar todo

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg.svg";
 import { connect } from 'react-redux'
 import { toggleCartHidden } from '../../redux/carrito/cart.actions';
+import { selectCartItemsCount } from '../../redux/carrito/cart.selectors';
 
 import "./cart-icon.scss";
 
@@ -15,8 +16,7 @@ const mapDispatchToProps =  dispatch => ({
   toggleCartHidden:() => dispatch(toggleCartHidden())
 })
 //mapstatetoprops para traer el state al icono y tomar el valor para mostrar
-const mapStateToProps = ({cart: {cartItems}}) =>({
-itemCount: cartItems.reduce(
-  (acumulado,cartItem) => acumulado + cartItem.cantidad,0) 
+const mapStateToProps = (state) =>({
+itemCount: selectCartItemsCount(state)
 })
 export default connect(mapStateToProps,mapDispatchToProps)(CartIcon) ;
---
+++ src/redux/carrito/cart.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from "reselect";
+//importo el input selector para tomar una porcion del state solamente, en este caso del carrito
+
+//funcion que retorna el state entero y retorna una porcion, una caoa de DB en geral
+// de todo el state del reducer solo queremos una pieza
+
+const selectCart = state => state.cart;
+//segundo selector para extraer solo la propiedad del carrito, recibe dos arg, uno un array de
+//input selectors el segundo arg es una funcion que devuelve el valor que queremnos del selector
+export const selectCartItems = createSelector([selectCart], cart =>cart.cartItems);
+//vas explotando de apoco hasta llegar al valor necesitado
+export const selectCartItemsCount = createSelector(
+    [selectCartItems], 
+    cartItems => cartItems.reduce(
+        (acumulado,cartItem) => acumulado + cartItem.cantidad,0) 
+        
+    
+    );
