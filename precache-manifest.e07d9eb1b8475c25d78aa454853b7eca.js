0c77909578f5c5313bc816fb7b95547e66d88690
Seba <morgado.s@utedyc.org.ar>
2020-03-10 13:07:47 -0300

    Updates

---
+++ precache-manifest.e07d9eb1b8475c25d78aa454853b7eca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a8619b3d538e026adac6c2017e0e46",
+    "url": "/CarroReact/index.html"
+  },
+  {
+    "revision": "7f34a1491e5f4ef19c6f",
+    "url": "/CarroReact/static/css/main.8341db17.chunk.css"
+  },
+  {
+    "revision": "9c1a6fb4998360bff359",
+    "url": "/CarroReact/static/js/2.aea9d253.chunk.js"
+  },
+  {
+    "revision": "29e353aceb7b5de4adcd15bc0e71c72b",
+    "url": "/CarroReact/static/js/2.aea9d253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f34a1491e5f4ef19c6f",
+    "url": "/CarroReact/static/js/main.acbccfaa.chunk.js"
+  },
+  {
+    "revision": "97b246042a0bfff4a7b0",
+    "url": "/CarroReact/static/js/runtime-main.15141ac8.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/CarroReact/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/CarroReact/static/media/shopping-bag.svg.30f07071.svg"
+  }
+]);
