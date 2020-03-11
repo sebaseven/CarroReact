f5d14dae47288ed5fce9a74ad3cb6ec986f8d3c5
Sebastian Morgado <smorgado@uade.edu.ar>
2020-03-10 22:58:05 -0300

    Persistence config e implementacion

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,19 @@
-import { combineReducers} from 'redux';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './carrito/cart.reducer';
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
