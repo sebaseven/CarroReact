4feb37c299460a5e59477324e640e420ff633902
Sebastian Morgado <smorgado@uade.edu.ar>
2020-03-22 22:17:21 -0300

    routeo junto con normalizacion de url en collection

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import './collections-overview.styles.scss';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-import './collections-overview.styles.scss';
+
+
 
 const CollectionsOverview = ({ collections }) => (
   <div className='collections-overview'>
@@ -17,7 +19,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollection
+  collections: selectCollections
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,10 +1,22 @@
-import { createSelector } from 'reselect';
+import { createSelector } from "reselect";
 
-const selectShop= state => state.shop;
+const selectShop = state => state.shop;
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+};
 
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
 
-
+export const selectCollection = collectionURLparam =>
+  createSelector([selectCollections], collections =>
+    collections.find(
+      collection => collection.id === COLLECTION_ID_MAP[collectionURLparam]
+    )
+  );
